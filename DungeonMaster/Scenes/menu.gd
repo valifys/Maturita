91159d433e46743995f40cc57e006d359ac4e13e
valifys <valousekj@gmail.com>
2025-03-12 20:17:17 +0100

    zmeny

--- DungeonMaster/Scenes/menu.gd
+++ DungeonMaster/Scenes/menu.gd
@@ -1,10 +1,9 @@
 extends Control
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
--- DungeonMaster/Scripts/player.gd
+++ DungeonMaster/Scripts/player.gd
@@ -37,3 +37,14 @@ func _physics_process(delta: float) -> void:
 
 
 	
+
+
+func _on_killzone_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		print("funguluje")
+		get_tree().change_scene_to_file("res://Scenes/menu.tscn")
